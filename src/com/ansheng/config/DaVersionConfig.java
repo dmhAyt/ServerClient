662b5f031a73a73ffdcab9921c8957564e6e10fa
dengan <dengmeihou_yt@163.com>
2020-12-25 11:25:07 +0800

    漏提交了da文件

---
+++ src/com/ansheng/config/DaVersionConfig.java
@@ -0,0 +1,9 @@
+package com.ansheng.config;
+
+public class DaVersionConfig {
+	public static String version = "1.0.0";
+	
+	public static String serverName = "文件管理客户端";
+	
+	public static String serverNameEn = "File_Manage_Client";
+}
